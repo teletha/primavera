ea5ee4eda09594cff9fc87e0d7dd7e36e8aa63f9
Teletha <Teletha.T@gmail.com>
2023-07-07 09:42:25 +0900

    initialize

---
+++ src/test/java/primavera/function/WrapperTriFunction.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 cointoss Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package primavera.function;
+
+import javax.annotation.processing.Generated;
+
+import primavera.SpecializedCodeGenerator.Primitive;
+
+@Generated("SpecializedCodeGenerator")
+public interface WrapperTriFunction {
+
+    /**
+     * Composes a single value from three arguments.
+     * 
+     * @param param1 First parameter.
+     * @param param2 Second parameter.
+     * @param param3 Third parameter.
+     * @return A calculated result.
+     */
+    Primitive applyAsWrapper(Primitive param1, Primitive param2, Primitive param3);
+}
